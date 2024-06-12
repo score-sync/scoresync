dabc667cf2770b2605b5be878dcdd2e08ae2a313
rakeshraih <rakeshraih@gmail.com>
2024-06-11 21:35:10 -0700

    fix: :bug: fix headre

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -9,20 +9,22 @@ const { SubMenu } = Menu;
 export const AppHeader = () => {
   return (
     <Header style={{ backgroundColor: '#fff', padding: 0 }}>
-      <Image src={Logo} className={'logo'} preview={false} style={{ height: '50px; !important' }} />
-      <Flex>
-        <h5>Welcome Sameer</h5>
-        <Menu onClick={console.log} selectedKeys={['mail']} mode="horizontal">
-          <SubMenu key="SubMenu" title={<MenuOutlined />}>
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-            <Menu.Item key="alipay">
-              <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                Navigation Four - Link
-              </a>
-            </Menu.Item>
-          </SubMenu>
-        </Menu>
+      <Flex justify="space-between">
+        <Image src={Logo} className={'logo'} preview={false} style={{ height: '50px; !important' }} />
+        <Flex>
+          <h5>Welcome Sameer</h5>
+          <Menu onClick={console.log} selectedKeys={['mail']} mode="horizontal">
+            <SubMenu key="SubMenu" title={<MenuOutlined />}>
+              <Menu.Item key="setting:1">Option 1</Menu.Item>
+              <Menu.Item key="setting:2">Option 2</Menu.Item>
+              <Menu.Item key="alipay">
+                <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                  Navigation Four - Link
+                </a>
+              </Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Flex>
       </Flex>
     </Header>
   );
