4ca1b862b02106786fcc04974cdbaab197692c40
rakeshraih <rakeshraih@gmail.com>
2024-06-16 11:11:30 -0700

    fix: :sparkles: add form fields

--- src/pages/Leage/CreateLague.tsx
+++ src/pages/Leage/CreateLague.tsx
@@ -1,53 +1,107 @@
-import { Input, Button, Upload, DatePicker, Form, InputNumber } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import { Input, Button, Upload, DatePicker, Form, InputNumber, Space, Flex } from 'antd';
+import { MinusOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons';
+import { useCallback } from 'react';
+
+const defaultValues = {
+  amateurMMABouts: 0,
+  proMMABouts: 0,
+  amateurBJJBouts: 0,
+  proBJJBouts: 0,
+  amateurK1Bouts: 0,
+  proK1Bouts: 0,
+};
 
 const CreateLague = () => {
+  const [form] = Form.useForm();
+
+  const increment = useCallback(
+    (field: string) => {
+      const currentValue = form.getFieldValue(field) ?? 0;
+      console.log(field, currentValue);
+      form.setFieldsValue({ [field]: parseInt(currentValue) + 1 });
+    },
+    [form],
+  );
+
+  const decrement = useCallback(
+    (field: string) => {
+      const currentValue = form.getFieldValue(field) ?? 0;
+      form.setFieldsValue({ [field]: Math.max(parseInt(currentValue) - 1, 0) });
+    },
+    [form],
+  );
+  console.log('Rerender', form.getFieldValue('amateurMMABouts'));
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-3xl">
+    <div className="flex flex-col items-center justify-center mt-8">
+      <div className="rounded-lg w-full">
         <h1 className="text-center text-xl font-bold mb-6">Create League</h1>
-        <Form layout="vertical">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {form.getFieldValue('amateurMMABouts')}
+        <Form form={form} layout="vertical" name="form-name" initialValues={defaultValues} onFinish={() => {}}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
             <Form.Item name="leagueName" label="League Name">
-              <Input />
+              <Input size="large" className="border-blue" />
             </Form.Item>
             <Form.Item name="leagueDateTime" label="League Date & Time">
-              <DatePicker showTime />
+              <DatePicker showTime size="large" className="border-blue" />
             </Form.Item>
             <Form.Item name="leagueLocation" label="League Location">
-              <Input />
+              <Input size="large" className="border-blue" />
             </Form.Item>
             <Form.Item name="promoterName" label="Promoter Name">
-              <Input />
+              <Input size="large" className="border-blue" />
             </Form.Item>
             <Form.Item name="amateurMMABouts" label="No. of Amateur MMA Bouts">
-              <InputNumber min={0} defaultValue={18} className="w-full" />
+              <Space>
+                <Button icon={<MinusOutlined />} onClick={() => decrement('amateurMMABouts')} />
+                <Input className="w-full border-blue" />
+                <Button icon={<PlusOutlined />} onClick={() => increment('amateurMMABouts')} />
+              </Space>
             </Form.Item>
             <Form.Item name="proMMABouts" label="No. of Pro MMA Bouts">
-              <InputNumber min={0} defaultValue={18} className="w-full" />
+              <Space>
+                <Button icon={<MinusOutlined />} onClick={() => decrement('proMMABouts')} />
+                <InputNumber min={0} className="w-full border-blue" />
+                <Button icon={<PlusOutlined />} onClick={() => increment('proMMABouts')} />
+              </Space>
             </Form.Item>
             <Form.Item name="amateurBJJbouts" label="No. of Amateur BJJ Bouts">
-              <InputNumber min={0} defaultValue={18} className="w-full" />
+              <Space>
+                <Button icon={<MinusOutlined />} onClick={() => decrement('amateurBJJbouts')} />
+                <InputNumber min={0} className="w-full border-blue" />
+                <Button icon={<PlusOutlined />} onClick={() => increment('amateurBJJbouts')} />
+              </Space>
             </Form.Item>
             <Form.Item name="proBJJbouts" label="No. of Pro BJJ Bouts">
-              <InputNumber min={0} defaultValue={18} className="w-full" />
+              <Space>
+                <Button icon={<MinusOutlined />} onClick={() => decrement('proBJJbouts')} />
+                <InputNumber min={0} className="w-full border-blue" />
+                <Button icon={<PlusOutlined />} onClick={() => increment('proBJJbouts')} />
+              </Space>
             </Form.Item>
             <Form.Item name="amateurK1bouts" label="No. of Amateur K1 Bouts">
-              <InputNumber min={0} defaultValue={18} className="w-full" />
+              <Space>
+                <Button icon={<MinusOutlined />} onClick={() => decrement('amateurK1bouts')} />
+                <InputNumber min={0} className="w-full border-blue" />
+                <Button icon={<PlusOutlined />} onClick={() => increment('amateurK1bouts')} />
+              </Space>
             </Form.Item>
             <Form.Item name="uploadLogo" label="Upload League Logo">
               <Upload>
-                <Button icon={<UploadOutlined />}>Upload</Button>
+                <Button icon={<UploadOutlined />} size="large">
+                  Upload
+                </Button>
               </Upload>
             </Form.Item>
           </div>
-          <div className="flex justify-between mt-6">
-            <Button>Back</Button>
-            <Button type="primary" htmlType="submit">
+          <Flex justify="space-between" className="mt-6">
+            <Button size="large">Back</Button>
+            <Button type="primary" htmlType="submit" size="large">
               Save
             </Button>
-            <Button>Next</Button>
-          </div>
+            <Button size="large" disabled>
+              Next
+            </Button>
+          </Flex>
         </Form>
       </div>
     </div>
